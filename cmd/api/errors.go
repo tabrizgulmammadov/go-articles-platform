05cd7b34a9a14140df36cf44c636fab354335d73
tabriz.gulmammadov <tabriz.gulmammadov@azai.tech>
2025-02-17 14:48:36 +0400

    feat: Add logging for the project

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,25 +6,25 @@ import (
 )
 
 func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	app.logger.Errorw("internal server error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
 
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	app.logger.Warnf("bad request error", "method", r.Method, "path", r.URL.Path, "error", err)
 
 	writeJSONError(w, http.StatusBadRequest, err.Error())
 }
 
 func (app *application) conflictResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("conflict error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	app.logger.Errorf("conflict error", "method", r.Method, "path", r.URL.Path, "error", err)
 
 	writeJSONError(w, http.StatusConflict, err.Error())
 }
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	app.logger.Warnf("not found error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
 
 	writeJSONError(w, http.StatusNotFound, "resource not found")
 }
